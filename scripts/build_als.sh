b6ce681e28365f02e0931740b3b1150b924ea7d2
Maxim Reznik <reznikmm@gmail.com>
2024-07-23 19:24:19 +0300

    Apply fixes for `shellcheck` warnings

--- scripts/build_als.sh
+++ scripts/build_als.sh
@@ -57,86 +57,86 @@ export PRETTIER_ADA_BUILD_MODE=prod
 
 # Install custom Alire index with missing crates
 function install_index() {
-    alr index --del=als || true
-    alr index --add=$PWD --name=als
+   alr index --del=als || true
+   alr index --add="$PWD" --name=als
 }
 
 # Clone dependencies
 function pin_crates() {
-    for crate in $PINS; do
-        repo_var=repo_$crate
-        branch_var=branch_$crate
+   for crate in $PINS; do
+      repo_var=repo_$crate
+      branch_var=branch_$crate
 
-        repo=${!repo_var}
-        branch=${!branch_var}
+      repo=${!repo_var}
+      branch=${!branch_var}
 
-        URL="https://github.com/AdaCore/${repo:-$crate}.git"
-        GIT="git clone --depth=1"
-        $GIT -b ${branch:-master} $URL subprojects/$crate
-        cp -v subprojects/$crate.toml subprojects/$crate/alire.toml
-        alr --force --non-interactive pin $crate --use=$PWD/subprojects/$crate
-    done
-    # Install gprbuild from our index. We need it for Mac OS X ARM64
-    alr toolchain --select gprbuild^24
-    alr action -r post-fetch # Configure XmlAda, etc
+      URL="https://github.com/AdaCore/${repo:-$crate}.git"
+      GIT="git clone --depth=1"
+      $GIT -b "${branch:-master}" "$URL" "subprojects/$crate"
+      cp -v "subprojects/$crate".toml "subprojects/$crate/alire.toml"
+      alr --force --non-interactive pin "$crate" "--use=$PWD/subprojects/$crate"
+   done
+   # Install gprbuild from our index. We need it for Mac OS X ARM64
+   alr toolchain --select gprbuild^24
+   alr action -r post-fetch # Configure XmlAda, etc
 }
 
 # Build langkit shared libraries required to generate libadalang.
 # Export setenv to $SETENV
 # Clean `.ali` and `.o` to avoid static vis relocatable mess
 function build_so_raw() {
-    cd subprojects/langkit_support
-    sed -i.bak -e 's/GPR_BUILD/GPR_LIBRARY_TYPE/' ./langkit/libmanage.py
-    pip install .
-    python manage.py make --no-mypy \
-        --library-types=relocatable --gargs "-cargs -fPIC"
-    python manage.py setenv >$SETENV
-    cd -
-    find . -name '*.o' -delete
-    find . -name '*.ali' -delete
+   cd subprojects/langkit_support
+   sed -i.bak -e 's/GPR_BUILD/GPR_LIBRARY_TYPE/' ./langkit/libmanage.py
+   pip install .
+   python manage.py make --no-mypy \
+      --library-types=relocatable --gargs "-cargs -fPIC"
+   python manage.py setenv >"$SETENV"
+   cd -
+   find . -name '*.o' -delete
+   find . -name '*.ali' -delete
 }
 
 # Run build_so_raw in Alire environment
 function build_so() {
-    LIBRARY_PATH=relocatable alr exec $0 -- build_so_raw
+   LIBRARY_PATH=relocatable alr exec "$0" -- build_so_raw
 }
 
 # Build ALS with alire
 function build_als() {
-    ADALIB=$(dirname "$(alr exec gcc -- -print-libgcc-file-name)")/adalib
-    LIBGCC=$(dirname "$(alr exec gcc -- -print-file-name=libgcc_s.so.1)")
-    DEPS=$PWD/alire/cache/dependencies
-    export DYLD_LIBRARY_PATH=$ADALIB:$DYLD_LIBRARY_PATH
-    export DYLD_LIBRARY_PATH=$LIBGCC:$DYLD_LIBRARY_PATH
-    export DYLD_LIBRARY_PATH=$DEPS/gnatcoll_iconv_24.0.0_e90c5b4d/iconv/lib/relocatable:$DYLD_LIBRARY_PATH
-    export DYLD_LIBRARY_PATH=$DEPS/gnatcoll_gmp_24.0.0_e90c5b4d/gmp/lib/relocatable:$DYLD_LIBRARY_PATH
-    export DYLD_LIBRARY_PATH=$DEPS/xmlada_24.0.0_ae5a015b/schema/lib/relocatable:$DYLD_LIBRARY_PATH
-    export DYLD_LIBRARY_PATH=$DEPS/xmlada_24.0.0_ae5a015b/dom/lib/relocatable:$DYLD_LIBRARY_PATH
-    export DYLD_LIBRARY_PATH=$DEPS/xmlada_24.0.0_ae5a015b/sax/lib/relocatable:$DYLD_LIBRARY_PATH
-    export DYLD_LIBRARY_PATH=$DEPS/xmlada_24.0.0_ae5a015b/input_sources/lib/relocatable:$DYLD_LIBRARY_PATH
-    export DYLD_LIBRARY_PATH=$DEPS/xmlada_24.0.0_ae5a015b/unicode/lib/relocatable:$DYLD_LIBRARY_PATH
+   ADALIB=$(dirname "$(alr exec gcc -- -print-libgcc-file-name)")/adalib
+   LIBGCC=$(dirname "$(alr exec gcc -- -print-file-name=libgcc_s.so.1)")
+   DEPS=$PWD/alire/cache/dependencies
+   export DYLD_LIBRARY_PATH=$ADALIB:$DYLD_LIBRARY_PATH
+   export DYLD_LIBRARY_PATH=$LIBGCC:$DYLD_LIBRARY_PATH
+   export DYLD_LIBRARY_PATH=$DEPS/gnatcoll_iconv_24.0.0_e90c5b4d/iconv/lib/relocatable:$DYLD_LIBRARY_PATH
+   export DYLD_LIBRARY_PATH=$DEPS/gnatcoll_gmp_24.0.0_e90c5b4d/gmp/lib/relocatable:$DYLD_LIBRARY_PATH
+   export DYLD_LIBRARY_PATH=$DEPS/xmlada_24.0.0_ae5a015b/schema/lib/relocatable:$DYLD_LIBRARY_PATH
+   export DYLD_LIBRARY_PATH=$DEPS/xmlada_24.0.0_ae5a015b/dom/lib/relocatable:$DYLD_LIBRARY_PATH
+   export DYLD_LIBRARY_PATH=$DEPS/xmlada_24.0.0_ae5a015b/sax/lib/relocatable:$DYLD_LIBRARY_PATH
+   export DYLD_LIBRARY_PATH=$DEPS/xmlada_24.0.0_ae5a015b/input_sources/lib/relocatable:$DYLD_LIBRARY_PATH
+   export DYLD_LIBRARY_PATH=$DEPS/xmlada_24.0.0_ae5a015b/unicode/lib/relocatable:$DYLD_LIBRARY_PATH
 
-    LIBRARY_TYPE=static STANDALONE=no alr exec make -- VERSION=$TAG check
+   LIBRARY_TYPE=static STANDALONE=no alr exec make -- "VERSION=$TAG" check
 }
 
 # Find the path to libgmp as linked in the given executable
 function get_gmp_full_path() {
-    otool -l "$1" | grep '^\s*name.*libgmp.10.dylib' | awk '/ name /{print $2 }'
+   otool -l "$1" | grep '^\s*name.*libgmp.10.dylib' | awk '/ name /{print $2 }'
 }
 
 function do_fix_rpath() {
-    # Remove all rpath entries
-    for R in $(otool -l "$1" | grep -A2 LC_RPATH | awk '/ path /{ print $2 }'); do
-        install_name_tool -delete_rpath "$R" "$1"
-    done
-    # Change reference to full path of libgmp into a reference to the rpath.
-    gmp_full_path=$(get_gmp_full_path "$1")
-    if [ -n "$gmp_full_path" ]; then
-        install_name_tool -change "$gmp_full_path" @rpath/libgmp.10.dylib "$1"
-    fi
-    # Add the executable directory to rpath so it can find shared libraries
-    # packaged alongside the executable.
-    install_name_tool -add_rpath @executable_path "$1"
+   # Remove all rpath entries
+   for R in $(otool -l "$1" | grep -A2 LC_RPATH | awk '/ path /{ print $2 }'); do
+      install_name_tool -delete_rpath "$R" "$1"
+   done
+   # Change reference to full path of libgmp into a reference to the rpath.
+   gmp_full_path=$(get_gmp_full_path "$1")
+   if [ -n "$gmp_full_path" ]; then
+      install_name_tool -change "$gmp_full_path" @rpath/libgmp.10.dylib "$1"
+   fi
+   # Add the executable directory to rpath so it can find shared libraries
+   # packaged alongside the executable.
+   install_name_tool -add_rpath @executable_path "$1"
 }
 
 # Get architecture and platform information from node.
@@ -145,42 +145,42 @@ NODE_ARCH=$(node -e "console.log(process.arch)")
 ALS_EXEC_DIR=integration/vscode/ada/$NODE_ARCH/$NODE_PLATFORM
 
 function fix_rpath() {
-    if [ "$RUNNER_OS" = macOS ]; then
-        # Get full path of libgmp as linked in the ALS exec
-        gmp_full_path=$(get_gmp_full_path $ALS_EXEC_DIR/ada_language_server)
-        if [ -f "$gmp_full_path" ]; then
-            # Copy libgmp alongside the ALS exec
-            cp -v -f "$gmp_full_path" "$ALS_EXEC_DIR"
-        fi
-        # Fix rpath entries of the ALS exec so it can find libgmp alongside it
-        do_fix_rpath "$ALS_EXEC_DIR/ada_language_server"
-    fi
+   if [ "$RUNNER_OS" = macOS ]; then
+      # Get full path of libgmp as linked in the ALS exec
+      gmp_full_path=$(get_gmp_full_path "$ALS_EXEC_DIR/ada_language_server")
+      if [ -f "$gmp_full_path" ]; then
+         # Copy libgmp alongside the ALS exec
+         cp -v -f "$gmp_full_path" "$ALS_EXEC_DIR"
+      fi
+      # Fix rpath entries of the ALS exec so it can find libgmp alongside it
+      do_fix_rpath "$ALS_EXEC_DIR/ada_language_server"
+   fi
 }
 
 function strip_debug() {
-    cd "$ALS_EXEC_DIR"
+   cd "$ALS_EXEC_DIR"
 
-    if [ "$RUNNER_OS" = Windows ]; then
-        ALS=ada_language_server.exe
-    else
-        ALS=ada_language_server
-    fi
+   if [ "$RUNNER_OS" = Windows ]; then
+      ALS=ada_language_server.exe
+   else
+      ALS=ada_language_server
+   fi
 
-    if [ "$RUNNER_OS" = macOS ]; then
-        # On macOS using objcopy from binutils to strip debug symbols to a
-        # separate file doesn't work. Namely, the last step `objcopy
-        # --add-gnu-debuglink` yields an executable that crashes at startup.
-        #
-        # Instead we use dsymutil and strip which are commands provided by the
-        # system (or by XCode).
-        dsymutil "$ALS"
-        strip "$ALS"
-    else
-        objcopy --only-keep-debug ${ALS} ${ALS}.debug
-        objcopy --strip-all ${ALS}
-        objcopy --add-gnu-debuglink=${ALS}.debug ${ALS}
-    fi
-    cd -
+   if [ "$RUNNER_OS" = macOS ]; then
+      # On macOS using objcopy from binutils to strip debug symbols to a
+      # separate file doesn't work. Namely, the last step `objcopy
+      # --add-gnu-debuglink` yields an executable that crashes at startup.
+      #
+      # Instead we use dsymutil and strip which are commands provided by the
+      # system (or by XCode).
+      dsymutil "$ALS"
+      strip "$ALS"
+   else
+      objcopy --only-keep-debug ${ALS} ${ALS}.debug
+      objcopy --strip-all ${ALS}
+      objcopy --add-gnu-debuglink=${ALS}.debug ${ALS}
+   fi
+   cd -
 }
 
 # Setup venv for python
@@ -191,39 +191,39 @@ pip3 install e3-testsuite
 
 case ${1:-all} in
 all)
-    install_index
-    pin_crates
-    build_so
-    build_als
-    fix_rpath
-    strip_debug
-    ;;
+   install_index
+   pin_crates
+   build_so
+   build_als
+   fix_rpath
+   strip_debug
+   ;;
 
 install_index)
-    install_index
-    ;;
+   install_index
+   ;;
 
 pin_crates)
-    pin_crates
-    ;;
+   pin_crates
+   ;;
 
 build_so)
-    build_so
-    ;;
+   build_so
+   ;;
 
 build_so_raw)
-    build_so_raw
-    ;;
+   build_so_raw
+   ;;
 
 build_als)
-    build_als
-    ;;
+   build_als
+   ;;
 
 fix_rpath)
-    fix_rpath
-    ;;
+   fix_rpath
+   ;;
 
 strip_debug)
-    strip_debug
-    ;;
+   strip_debug
+   ;;
 esac
